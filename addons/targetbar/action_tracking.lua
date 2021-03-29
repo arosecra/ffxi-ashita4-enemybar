3eac6ec5bb1f7f20f9374e01eee392b4380c491c
arosecra <aaron.rosecrans@gmail.com>
2021-03-29 00:35:35 -0500

    initial version

---
+++ addons/targetbar/action_tracking.lua
@@ -0,0 +1,137 @@
+local action = require("org_github_arosecra.packets.action");
+
+
+local action_tracking = {};
+
+local starting_message_ids = T{IActionMagicStart,IActionWeaponskillStart,IActionItemStart}
+local completed_message_ids = T{IActionMagicFinish,IActionMobAbilityFinish,IActionWeaponskillFinish,IActionItemFinish,IActionJobAbilityFinish}
+local spell_message_ids = T{IActionMagicStart}
+local item_message_ids = T{IActionItemStart}
+local weapon_skill_message_ids = T{IActionWeaponskillStart}
+local tracked_debuff_ids = T{2,19,7,28}
+local untracked_debuff_categories = T{8,7,6,9}
+local damaging_spell_message_ids = T{2,252}
+local non_damaging_spell_message_ids = T{75,236,237,268,270,271}
+
+action_tracking.track = function(action, runtime_config)
+	local target = AshitaCore:GetMemoryManager():GetTarget();
+    --print('----')
+    --print('category: ' .. action.category);
+    --print('target_count: ' .. action.target_count);
+    --print('actor_id: ' .. action.actor_id);
+    --print('memory target id: ' .. target:GetTargetIndex(0))
+    --print('param: ' .. action.param);
+    --if(action.targets[1] ~= nil) then
+    --    print('target[1].id: ' .. action.targets[1].id)
+    --    print('target[1].action_count: ' .. action.targets[1].action_count)
+    --    if(action.targets[1].actions[1] ~= nil) then
+    --        print('target action.param: ' .. action.targets[1].actions[1].param)
+    --        print('target action.message: ' .. action.targets[1].actions[1].message)
+    --    end
+    --end
+    action_tracking.track_action(action, runtime_config);
+    action_tracking.track_enemy_target(action, runtime_config);
+    action_tracking.track_debuff(action, runtime_config);
+end
+
+action_tracking.track_action = function(action, runtime_config) 
+    local actor_id = action.actor_id
+
+    if action.actor_id ~= runtime_config.ServerId then
+        return;
+    end
+
+    local action_id = action.param;
+    if starting_message_ids:contains(action.category) then
+        if action.targets[1] ~= nil and action.targets[1].actions[1] ~= nil then
+        	action_id = action.targets[1].actions[1].param;
+        end
+    end
+
+    if action_id == 0 then
+        return;
+    end
+
+    local action_name = nil;
+    if spell_message_ids:contains(action.category) then
+        local spell = AshitaCore:GetResourceManager():GetSpellById(action_id);
+        if spell ~= nil then
+            runtime_config.Action = spell.Name[1];
+        else
+            runtime_config.Action = 'Unknown Spell ' .. action_id;
+        end
+        runtime_config.ActionStartTime = os.time();
+    elseif item_message_ids:contains(action.category) then
+        local item = AshitaCore:GetResourceManager():GetItemById(action_id);
+        if item ~= nil then
+            runtime_config.Action = item.Name[2];
+        else
+            runtime_config.Action = 'Unknown Item ' .. action_id;
+        end
+        runtime_config.ActionStartTime = os.time();
+    elseif actor_id < 0x01000000 then --npcs
+        local ability = AshitaCore:GetResourceManager():GetString("mobskills", action_id, 2);
+        if ability ~= nil then
+            runtime_config.Action = ability;
+        else
+            runtime_config.Action = 'Unknown NPC Ability ' .. action_id;
+        end
+        runtime_config.ActionStartTime = os.time();
+    elseif action.category == IActionJobAbilityFinish then
+        local ability = AshitaCore:GetResourceManager():GetAbilityById(action_id);
+        if ability ~= nil then
+            runtime_config.Action = ability.Name[2];
+        else
+            runtime_config.Action = 'Unknown Ability ' .. action_id;
+        end
+        runtime_config.ActionStartTime = os.time();
+    elseif weapon_skill_message_ids:contains(action.category) then
+        local ability = AshitaCore:GetResourceManager():GetAbilityById(action_id);
+        if ability ~= nil then
+            runtime_config.Action = ability.Name[2];
+        else
+            runtime_config.Action = 'Unknown Weaponskill ' .. action_id;
+        end
+        runtime_config.ActionStartTime = os.time();
+    end
+
+    if action.targets[1] ~= nil and 
+       action.targets[1].message == 0 and
+       action.targetsp[1].id == action.actor_id then
+        runtime_config.Action = "";
+    end
+end
+
+-- track the target of the target, if there is one
+action_tracking.track_enemy_target = function(action, runtime_config)
+    local actor_id = action.actor_id
+
+    if action.actor_id ~= runtime_config.ServerId then
+        return;
+    end
+
+    -- if there's more than one target or no target, ignore the action
+    if action.target_count > 1 and
+       action.target_count == 0 then
+        return;
+    end
+
+    -- we only follow auto attack actions to track the target of the target
+    if action.category ~= IActionAttack then
+        return;
+    end
+
+    runtime_config.TargetName = runtime_config.PartyServerIds[action.targets[1].id];
+
+
+end
+
+action_tracking.track_debuff = function(action, runtime_config)
+    if untracked_debuff_categories:contains(action.category) then
+        return;
+    end
+
+    --track enemy buffs (rino guard, for instance)
+end
+
+return action_tracking;
---
+++ addons/targetbar/targetbar.lua
@@ -0,0 +1,160 @@
+
+addon.name      = 'targetbar';
+addon.author    = 'arosecra';
+addon.version   = '1.0';
+addon.desc      = '';
+addon.link      = '';
+
+local imgui = require('imgui');
+local common = require('common');
+
+local libs2imgui = require('org_github_arosecra/imgui');
+local libs2config = require('org_github_arosecra/config');
+local jobs = require('org_github_arosecra/jobs');
+local mechanics = require('org_github_arosecra/mechanics');
+local action = require('org_github_arosecra/packets/action');
+local action_tracking = require('action_tracking');
+
+
+local targetbar_window = {
+    is_open = { true }
+};
+
+-- runtime_config = {
+--    Name
+--    Percent
+--    ServerId
+--    Index
+--    Action  = {
+--       Name
+--       Start Time
+--       Ended
+--    }
+--    PartyServerIds = {
+--
+--    }
+--    TargetName
+--}
+
+local runtime_config = {
+};
+
+local once = false;
+
+ashita.events.register('load', 'targetbar_load_cb', function ()
+    print("[targetbar] 'load' event was called.");
+	local playerEntity = GetPlayerEntity();
+	print(playerEntity.ServerId);
+	print(AshitaCore:GetMemoryManager():GetParty():GetMemberTargetIndex(0));
+	--AshitaCore:GetConfigurationManager():Load(addon.name, 'targetbar\\targetbar.ini');
+	--runtime_config.tab = AshitaCore:GetConfigurationManager():GetString(addon.name, "settings", "defaulttab");
+	--runtime_config.tab_type = AshitaCore:GetConfigurationManager():GetString(addon.name, "settings", "tabs.type." .. runtime_config.tab);
+end);
+
+ashita.events.register('command', 'targetbar_command_cb', function (e)
+    if (not e.command:startswith('/targetbar') and not e.command:startswith('/tb')) then
+		return;
+    end
+    print("[targetbar] Blocking '/tb' command!");
+    e.blocked = true;
+end);
+
+ashita.events.register('plugin_event', 'targetbar_plugin_event_cb', function (e)
+    if (not e.name:startswith('/targetbar') and not e.name:startswith('/tb')) then
+		return;
+    end
+    print("[targetbar] Blocking '/tb' command!");
+    e.blocked = true;
+	
+end);
+
+local i = 0;
+
+ashita.events.register('packet_in', 'targetbar_packet_in_callback1', function (e)
+    if (e.id == IPacketIncomingAction) then
+		local action = action.parse(e.data);
+		action_tracking.track(action, runtime_config);
+
+		if once then
+			coroutine.sleepf(1);
+			local file = io.open("D:\\action_" .. i .. ".txt", "w")
+			file:write(e.data);
+			file:close();
+			i = i + 1;
+			once = true;
+		end
+    end
+end);
+
+--determine the targeted entity name, index, server id and hp percent
+ashita.events.register('d3d_beginscene', 'targetbar_beginscene_callback1', function (isRenderingBackBuffer)
+	local playerEntity = GetPlayerEntity();
+	if playerEntity == nil then
+		runtime_config = {};
+		return;
+	end
+	local party = AshitaCore:GetMemoryManager():GetParty();
+	local target = AshitaCore:GetMemoryManager():GetTarget();
+	local targetEntity = GetEntity(target:GetTargetIndex(0));
+	
+	if targetEntity == nil then
+		runtime_config = {};
+		return;
+	end
+	
+	runtime_config.Target = targetEntity.Name;
+	runtime_config.Percent = targetEntity.HPPercent / 100
+	
+	if runtime_config.Percent == 0 then
+		runtime_config = {};
+		return;
+	end
+	runtime_config.PartyServerIds = {}
+	for i=0,5 do
+		runtime_config.PartyServerIds[party:GetMemberServerId(i)] = party:GetMemberName(i);
+	end
+	
+	runtime_config.Index = targetEntity.TargetIndex
+	runtime_config.ServerId = targetEntity.ServerId
+
+	if runtime_config.ActionStartTime ~= nil and runtime_config.ActionStartTime - os.time() >= 3 then
+		runtime_config.ActionStartTime = nil;
+		runtime_config.Action = nil;
+	end
+
+end);
+
+-- draw the target bar table and progress bar
+ashita.events.register('d3d_present', 'targetbar_present_cb', function ()
+	local windowStyleFlags = libs2imgui.gui_style_table_to_var("imguistyle", addon.name, "window.style");
+	local tableStyleFlags = libs2imgui.gui_style_table_to_var("imguistyle", addon.name, "table.style");
+	libs2imgui.imgui_set_window(addon.name);
+	
+	if runtime_config.Index ~= nil then
+		if imgui.Begin(addon.name, targetbar_window.is_open, windowStyleFlags) then
+
+			if imgui.BeginTable(addon.name, 4, tableStyleFlags, 0, 0) then
+				imgui.TableNextColumn();
+				imgui.Text(runtime_config.Target);
+				imgui.TableNextColumn();
+				imgui.Text(tostring(runtime_config.Percent * 100));
+				imgui.TableNextColumn();
+				if runtime_config.Action ~= nil then
+					imgui.Text(runtime_config.Action);
+				end
+				imgui.TableNextColumn();
+				if runtime_config.TargetName ~= nil then
+					imgui.Text(runtime_config.TargetName);
+				end
+				
+				imgui.EndTable();
+			end
+			
+			local color = libs2imgui.get_color('health', mechanics.health_percent_to_status(runtime_config.Percent));
+			imgui.PushStyleColor(ImGuiCol_PlotHistogram, color);
+			imgui.ProgressBar(runtime_config.Percent, {-1.0, 0.0}, "");
+			imgui.PopStyleColor();
+		end
+		imgui.End();
+	end
+end);
